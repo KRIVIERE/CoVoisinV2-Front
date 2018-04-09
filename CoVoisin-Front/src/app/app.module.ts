9f56a4927c5962616a38bcf8f10db5d82a50d6c9
KRIVIERE <kvin_riviere@yahoo.fr>
2018-04-09 09:58:56 +0200

    Ajout du module pour l'index

--- CoVoisin-Front/src/app/app.module.ts
+++ CoVoisin-Front/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ConnexionComponent } from './index/connexion/connexion.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnexionComponent
   ],
   imports: [
     BrowserModule
