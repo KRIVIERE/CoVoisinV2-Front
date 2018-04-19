579b1d91504487200510825cf7f1e60c33ad3211
jean-christian <jc.vizzi@free.fr>
2018-04-19 09:18:20 +0200

    Complément pour la branche cascadeDelete, il manquait les fichiers services de requests.

---
+++ CoVoisin-Front/src/app/requests.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { HTTPClientHelper } from '../HTTPClientHelper';
+
+const HttpOptions = {
+  headers: new HttpHeaders({'content-type': 'application/json'})//Sera utilisé par les requêtes qui envoient du json
+}
+
+const baseURL = HTTPClientHelper.baseURL;
+
+@Injectable()
+export class RequestsService {
+
+  constructor(private http: HttpClient) { }
+
+  getRequests() {
+    return this.http.get(`${baseURL}/api/requests`);
+  }
+
+  getRequest(id) {
+    return this.http.get(`${baseURL}/api/requests/` + id);
+  }
+
+  deleteRequest(request) {
+    return this.http.delete(`${baseURL}/api/requests/` + request.id);
+  }
+
+    createRequest(request) {
+    let body = JSON.stringify(request);
+    return this.http.post(`${baseURL}/api/requests`, body, HttpOptions);
+  }
+
+  updateRequest(request) {
+    let body = JSON.stringify(request);
+    return this.http.put(`${baseURL}/api/requests/` + request.id, body, HttpOptions);
+  }
+
+}
